a12502de5857cef457765458d5498399c92b5e39
meeraameera <meerazack@gmail.com>
2026-09-04 16:31:42 +0800

    Dynamic Inputs

--- hello.py
+++ hello.py
@@ -3,15 +3,12 @@ print("Welcome here")
 print("My first post!")
 print("======================================")
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
+username = input("Enter your username: ")
+age = input("Enter your age: ")
+category = input("Enter Content category: ")
 
-followers += 50  
-print("Day 1:", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
+print("\nInstagram Profile")
+print("====================")
+print("Username: ", username)
+print("Age: ", age) 
+print("Category: ", category)
