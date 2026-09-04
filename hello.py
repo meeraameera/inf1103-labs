6244781b05ec9277c851283faab6cf1778e92f38
meeraameera <meerazack@gmail.com>
2026-09-04 16:51:25 +0800

    With Conditions

--- hello.py
+++ hello.py
@@ -1,10 +1,5 @@
-print("======================================")
-print("Welcome here")
-print("My first post!")
-print("======================================")
-
 username = input("Enter your username: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 category = input("Enter Content category: ")
 
 print("\nInstagram Profile")
@@ -12,3 +7,7 @@ print("====================")
 print("Username: ", username)
 print("Age: ", age) 
 print("Category: ", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
+    
